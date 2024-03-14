fb924974449f4a0a8044fe41e1136c3bc8e81621
AnuragDharmadhikari <106768147+AnuragDharmadhikari@users.noreply.github.com>
2024-03-14 13:20:26 +0530

    typo correction

--- src/forms/manage-resturant-form/ImageSection.tsx
+++ src/forms/manage-resturant-form/ImageSection.tsx
@@ -17,7 +17,7 @@ const ImageSection = () => {
       <div className="">
         <h2 className=" text-2xl font-bold">Image</h2>
         <FormDescription>
-          Add an image that will be displayed on ypur restaurant listing in the
+          Add an image that will be displayed on your restaurant listing in the
           search results. Adding a new image will overwrite the existing one.
         </FormDescription>
       </div>
